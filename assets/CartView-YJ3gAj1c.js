df8b902f05b8ae7f07be86885f8c7bda414c4cbc
Hsuan <welcome09210801@gmail.com>
2024-02-24 01:10:22 +0800

    Updates

---
+++ assets/CartView-YJ3gAj1c.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./index-X4FgiYGu.js";const t={};function n(o,a){return r(),c("h1",null,"這是前台購物車")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ assets/ProductView-9oDYm68F.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-X4FgiYGu.js";const r={};function t(n,s){return o(),c("h1",null,"這是產品")}const a=e(r,[["render",t]]);export{a as default};
